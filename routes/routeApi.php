0126678e554d3030f369e34259ee73d2c5e7a705
root <root@DESKTOP-Q8DD6FQ>
2024-04-10 15:30:04 -0300

    v2

--- routes/routeApi.php
+++ routes/routeApi.php
@@ -4,7 +4,9 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Teclib\UserController;
 
 Route::group(["prefix" => "v1"], function () {
+
     Route::group(["prefix" => "users"], function () {
-        Route::post('/consulta', [UserController::class, 'consulta']);
+        Route::get('/consulta', [UserController::class, 'consulta']);
     });
+    
 });
